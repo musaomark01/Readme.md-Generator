3f840aa73b0c82dffe5210234c8a18bfb92325dd
musaomark01 <musaomark01@gmail.com>
2023-10-05 03:28:16 -0400

    added the missing criterias

--- index.js
+++ index.js
@@ -14,10 +14,26 @@ inquirer
       message: 'Enter description for your project?',
     },
     {
+      type: 'confirm',
+      name: 'includeTableOfContents',
+      message: 'Do you want to add a table of contents?',
+      default: true,
+    },
+    {
+      type: 'checkbox',
+      name: 'tableOfContentsSections',
+      message: 'Select the sections you want to include in the table of contents:',
+      choices: [
+        'Installation', 'Usage', 'License', 'Contributing', 'Test','Questions',
+      ],
+      when: (answers) => answers.includeTableOfContents,
+    },
+    {
       type: 'input',
       name: 'installation',
       message: 'Enter installation instructions?',
     },
+    
     {
       type: 'input',
       name: 'usage',
@@ -25,34 +41,35 @@ inquirer
     },
     {
       type: 'input',
-      name: 'video',
-      message: 'Enter a link to a video explaining how to run your app?',
+      name: 'contributing',
+      message: '',
     },
     {
-        type: 'rawlist',
-  name: 'license',
-  message: 'What type of license are you using for this project?',
-  choices: [
-    'MIT License',
-    'GNU General Public License 3.0',
-    'Apache License 2.0',
-    'BSD 2-Clause License',
-    'BSD 3-Clause License',
-    'GNU Lesser General Public License 3.0',
-    'Mozilla Public License 2.0',
-    'ISC License',
-    'The Unlicense',
-    'Eclipse Public License 2.0',
-    'GNU Affero General Public License 3.0',
-    'GNU General Public License 2.0',
-    'Mozilla Public License 1.1',
-    'GNU Lesser General Public License 2.1',
-        ],
-      },
+      type: 'rawlist',
+      name: 'license',
+      message: 'What type of license are you using for this project?',
+      choices: [
+        'MIT License',
+        'GNU General Public License 3.0',
+        'Apache License 2.0',
+        'Boost Software License 1.0',
+        'BSD 2-Clause License',
+        'BSD 3-Clause License',
+        'GNU Lesser General Public License 3.0',
+        'Mozilla Public License 2.0',
+        'ISC License',
+        'The Unlicense',
+        'Eclipse Public License 2.0',
+        'GNU Affero General Public License 3.0',
+        'GNU General Public License 2.0',
+        'Mozilla Public License 1.1',
+        'GNU Lesser General Public License 2.1',
+      ],
+    },
     {
       type: 'input',
-      name: 'demo',
-      message: 'Enter a link to a demo?',
+      name: 'test',
+      message: 'Enter instructions on how to test your app?',
     },
     {
       type: 'input',
@@ -64,50 +81,99 @@ inquirer
       name: 'email',
       message: 'Enter your email address?',
     },
-    {
-      type: 'input',
-      name: 'instructions',
-      message: 'Enter special on how to reach you instructions?',
-    },
   ])
   .then((data) => {
-    const {
-      title, description, installation, usage, video, demo, license, github, email, instructions,
-    } = data;
+    const {  title, description, installation, usage, test, license, contributing, github, email, tableOfContentsSections,} = data;
+
+    let licenseBadgeURL = '';
+
+    switch (license) {
+      case 'MIT License':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-MIT-yellow.svg';
+        break;
+      case 'GNU General Public License 3.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-GPL%20v3-blue.svg';
+        break;
+      case 'Apache License 2.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-Apache%202.0-blue.svg';
+        break;
+      case 'Boost Software License 1.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg';
+        break;
+      case 'BSD 2-Clause License':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-BSD%202--Clause-orange.svg';
+        break;
+      case 'BSD 3-Clause License':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg';
+        break;
+      case 'GNU Lesser General Public License 3.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-LGPL%20v3-blue.svg';
+        break;
+      case 'Mozilla Public License 2.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg';
+        break;
+      case 'ISC License':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-ISC-blue.svg';
+        break;
+      case 'The Unlicense':
+        licenseBadgeURL = 'https://img.shields.io/badge/license-Unlicense-blue.svg';
+        break;
+      case 'Eclipse Public License 2.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-EPL%202.0-red.svg';
+        break;
+      case 'GNU Affero General Public License 3.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-AGPL%20v3-blue.svg';
+        break;
+      case 'GNU General Public License 2.0':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-GPL%20v2-blue.svg';
+        break;
+      case 'Mozilla Public License 1.1':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-MPL%201.1-brightgreen.svg';
+        break;
+      case 'GNU Lesser General Public License 2.1':
+        licenseBadgeURL = 'https://img.shields.io/badge/License-LGPL%202.1-blue.svg';
+        break;
+    }
+
+    let tableOfContents = '';
+
+    if (tableOfContentsSections && tableOfContentsSections.length > 0) {
+      tableOfContents += '## Table of Contents\n';
+      tableOfContentsSections.forEach((section) => {
+        tableOfContents += `- [${section}](#${section.toLowerCase()})\n`;
+      });
+    }
 
     const readmeMdContent = `
 # ${title}
 
+[![${license}](${licenseBadgeURL})](LICENSE)
+
+This project is licensed under ${license}
+
+
 ## Description
 ${description}
 
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Test](#test)
-- [Questions](#questions)
+${tableOfContents}
 
 ## Installation
 ${installation}
 
 ## Usage
 ${usage}
-[How to use video](${video})
 
-## License
-This project is licensed under the ${license} license.
+## License 
+This project is licensed under [${license}](License)
 
 ## Contributing
-
-## Test
-Deployed Application: [Demo](${demo})
+${contributing}
+## Test Instructions
+${test}
 
 ## Questions
-- GitHub: [${github}](https://github.com/${github})
-- Email: ${email}
-- Instructions: ${instructions}
+GitHub: [${github}](https://github.com/${github})
+For any additional questions, please feel free to contact me at ${email}.
 `;
 
     fs.writeFile('readme.md', readmeMdContent, (err) => {
